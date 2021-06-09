42648ae3c2a5bbb7a3d73d425ef3984f26b06de9
icboluo <1139803720@lp>
2021-06-09 13:44:05 +0800

    fund

--- src/view/FundData.vue
+++ src/view/FundData.vue
@@ -4,27 +4,82 @@
     <el-button type="primary" plain @click="init">
       搜索
     </el-button>
-    <el-button type="primary" plain @click="cal">
-      计算
+    <el-input v-model="simCal.source" placeholder="请输入source"></el-input>
+    <el-input v-model="simCal.target" placeholder="请输入target"></el-input>
+    <el-button type="primary" plain @click="httpSimCal">
+      计算增长率 {{ simCal.res }}
     </el-button>
 
-    <div>
-      incrIncr {{view.incrIncr}}
-    </div>
-    <div>
-      incrDecr {{view.incrDecr}}
-    </div>
-    <div>
-      decrIncr {{view.decrIncr}}
-    </div>
-    <div>
-      decrDecr {{view.decrDecr}}
-    </div>
+    <el-row :gutter="20">
+      <el-col :span="8">
+        <div class="grid-content bg-purple">
+          max {{ view.max }}
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <div class="grid-content bg-purple">
+          min {{ view.min }}
+        </div>
+      </el-col>
+      <el-col :span="8">
+        <div class="grid-content bg-purple">
+          avg {{ view.avg }}
+        </div>
+      </el-col>
+    </el-row>
 
-    count {{ view.count }}
-    max {{ view.max }}
-    min {{ view.min }}
-    avg {{ view.avg }}
+    <el-row :gutter="20">
+      <el-col :span="6">
+        <div class="grid-content bg-purple">
+          incrIncr {{ view.incrIncr }}
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple">
+          incrDecr {{ view.incrDecr }}
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple">
+          decrIncr {{ view.decrIncr }}
+        </div>
+      </el-col>
+      <el-col :span="6">
+        <div class="grid-content bg-purple">
+          decrDecr {{ view.decrDecr }}
+        </div>
+      </el-col>
+    </el-row>
+
+    <el-row :gutter="20">
+      <!--      一行总长24，平均分成5份-->
+      <el-col :span="5">
+        <div class="grid-content bg-purple">
+          monday {{ view.weekMap.MONDAY }}
+        </div>
+      </el-col>
+      <el-col :span="5">
+        <div class="grid-content bg-purple">
+          tuesday {{ view.weekMap.TUESDAY }}
+        </div>
+      </el-col>
+      <el-col :span="5">
+        <div class="grid-content bg-purple">
+          wednesday {{ view.weekMap.WEDNESDAY }}
+        </div>
+      </el-col>
+      <el-col :span="5">
+        <div class="grid-content bg-purple">
+          thursday {{ view.weekMap.THURSDAY }}
+        </div>
+      </el-col>
+      <el-col :span="4">
+        <div class="grid-content bg-purple">
+          friday {{ view.weekMap.FRIDAY }}
+        </div>
+      </el-col>
+    </el-row>
+
     <el-table
       :data="tableData"
       style="width: 100%">
@@ -46,16 +101,16 @@
         label="净值日期">
       </el-table-column>
       <el-table-column
+        prop="dayOfWeek"
+        label="星期">
+      </el-table-column>
+      <el-table-column
         prop="netAssetValue"
         label="单位净值">
       </el-table-column>
       <el-table-column
         prop="netValueCumulative"
         label="累计净值">
-      </el-table-column>
-      <el-table-column
-        prop="createTime"
-        label="创建时间">
       </el-table-column>
       <el-table-column
         prop="updateTime"
@@ -65,7 +120,7 @@
 
     <div class="block">
       <el-pagination
-        layout="prev, pager, next"
+        layout="total, sizes, prev, pager, next, jumper"
         :total="total"
         @current-change="handlerCurChange"
         :current-page.sync="currentPage">
@@ -92,7 +147,20 @@ export default {
         incrIncr: null,
         incrDecr: null,
         decrIncr: null,
-        decrDecr: null
+        decrDecr: null,
+        weekMap: {
+          MONDAY: null,
+          TUESDAY: null,
+          WEDNESDAY: null,
+          THURSDAY: null,
+          FRIDAY: null
+        },
+        monthMap: null
+      },
+      simCal: {
+        source: null,
+        target: null,
+        res: null
       }
     }
   },
@@ -126,6 +194,19 @@ export default {
         .then(r => {
           this.view = r.data.data
         })
+    },
+    httpSimCal () {
+      this.$axios.get(Common.fundUrlPre + '/fundData/simCal',
+        {
+          params: {
+            source: this.simCal.source,
+            target: this.simCal.target
+          }
+        }
+      )
+        .then(r => {
+          this.simCal.res = r.data.data
+        })
     }
   },
   mounted () {
@@ -135,5 +216,38 @@ export default {
 }
 </script>
 <style scoped>
+.el-row {
+  margin-bottom: 20px;
 
+&
+:last-child {
+  margin-bottom: 0;
+}
+
+}
+.el-col {
+  border-radius: 4px;
+}
+
+.bg-purple-dark {
+  background: #99a9bf;
+}
+
+.bg-purple {
+  background: #d3dce6;
+}
+
+.bg-purple-light {
+  background: #e5e9f2;
+}
+
+.grid-content {
+  border-radius: 4px;
+  min-height: 36px;
+}
+
+.row-bg {
+  padding: 10px 0;
+  background-color: #f9fafc;
+}
 </style>
