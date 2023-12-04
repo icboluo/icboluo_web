78bf52b624efbad9e0bf2b210a02fbb2e127cc25
icboluo <1139803720@qq.com>
2023-12-04 21:33:58 +0800

    add getCurrentInstance

--- src/components/BaseTable.vue
+++ src/components/BaseTable.vue
@@ -1,7 +1,5 @@
 <template>
-  <el-table :data="tableInfo?.data1"
-            v-bind="$attrs"
-            style="width: 100%">
+  <el-table :data="tableInfo?.data1" v-bind="$attrs" style="width: 100%">
     <el-table-column
       v-for="(item, idx) in tableInfo?.header"
       v-bind="item"
@@ -19,16 +17,17 @@
     <el-pagination
       v-bind="pageInfo"
       layout="total, sizes, prev, pager, next, jumper"
-      :page-sizes="pageInfo.pageSizes||[3,5,10,20]"
-      @size-change="(pageSize:number)=>handleSizeChange(pageSize,pageInfo)"
-      @current-change="(curPage:number)=>handleCurrentChange(curPage,pageInfo)"
+      :page-sizes="pageInfo.pageSizes || [3, 5, 10, 20]"
+      @size-change="(pageSize: number) => handleSizeChange(pageSize, pageInfo)"
+      @current-change="(curPage: number) => handleCurrentChange(curPage, pageInfo)"
     >
     </el-pagination>
   </div>
 </template>
 
 <script setup lang="ts">
-import type { PaginationProps, TableColumnCtx } from "element-plus";
+import type { PaginationProps, TableColumnCtx } from 'element-plus'
+import { ComponentInternalInstance, getCurrentInstance, onMounted } from 'vue'
 
 /**
  * 不可重复定义，相当于定义了一个全局变量
@@ -40,47 +39,59 @@ defineProps({
   pageInfo: {
     type: Object as () => PageInfo
   }
-});
+})
 
 /**
  * init 时间不可与 handlerSizeChange handlerCurChange 事件混用
  */
 const allEmits = defineEmits([
-  "handlerSizeChange",
-  "handlerCurChange",
-  "init"
-]);
+  'handlerSizeChange',
+  'handlerCurChange',
+  'init'
+])
+
+let inst: ComponentInternalInstance | null
+onMounted(() => {
+  inst = getCurrentInstance()
+})
 
 const handleSizeChange = (pageSize: number, pageInfo?: PageInfo) => {
   if (pageInfo) {
-    pageInfo.pageSize = pageSize;
+    pageInfo.pageSize = pageSize
   }
-  allEmits("init");
-  allEmits("handlerSizeChange", pageSize);
-};
+  if (inst.vnode.props.onHandlerSizeChange) {
+    allEmits('handlerSizeChange', pageSize)
+  } else {
+    allEmits('init')
+  }
+}
 const handleCurrentChange = (curPage: number, pageInfo?: PageInfo) => {
   if (pageInfo) {
-    pageInfo.pageNum = curPage;
+    pageInfo.pageNum = curPage
   }
-  allEmits("init");
-  allEmits("handlerCurChange", curPage);
-};
+  // 如果存在该事件，优先使用自定义的事件
+  if (inst.vnode.props.onHandlerCurChange) {
+    allEmits('handlerCurChange', curPage)
+  } else {
+    allEmits('init')
+  }
+}
 
 export interface TableInfo {
-  header: Header[];
-  data1: object[];
+  header: Header[]
+  data1: object[]
 }
 
 export interface Header extends TableColumnCtx<any> {
-  fieldName: string;
-  showName: string;
-  isButtonSlot: boolean;
+  fieldName: string
+  showName: string
+  isButtonSlot: boolean
 }
 
 export interface PageInfo extends PaginationProps {
-  total: number;
-  pageSize: number;
-  pageNum: number;
+  total: number
+  pageSize: number
+  pageNum: number
 }
 
 // 勿删，有使用
